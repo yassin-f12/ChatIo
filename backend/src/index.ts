1434958993d98a456d59eda9f50002c1d3338731
yassin-f12 <fah13.yous@gmail.com>
2026-07-01 14:53:01 +0200

    chore(backend): deployment render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import http from "http";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import authRoutes from './routes/auth.routes.js'
+import authRoutes from "./routes/auth.routes.js";
 import { initializeSocket } from "./socket/socket.js";
 dotenv.config();
 
@@ -12,13 +12,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/auth", authRoutes)
+app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
@@ -27,13 +27,11 @@ initializeSocket(server);
 connectDB()
   .then(() => {
     console.log("Database connect");
-    server.listen(PORT, () => {
-      console.log("Server is running on port ", PORT);
-    });
   })
   .catch((error) => {
-    console.log(
-      "failed to start server due to database connection error: ",
-      error,
-    );
+    console.log("failed DB:", error);
   });
+
+server.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
